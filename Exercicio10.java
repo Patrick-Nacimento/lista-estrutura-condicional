6b897638e166f3f8b306889dd12c603a48acb4a7
Patrick Otavio do Nacimento <patricknascimento198@gmail.com>
2018-04-20 09:44:59 -0300

    continuando o exercicio 9

--- Exercicio10.java
+++ Exercicio10.java
@@ -5,4 +5,15 @@ public class Exercicio10{
     public static void main(String[] args){
 
         int numero01 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
-        int numero02 = Integer.parseInt(JOptionPane.showInputDialog)
+        int numero02 = Integer.parseInt(JOptionPane.showInputDialog("Digite segundo numero"));
+        int numero03 = Integer.parseInt(JOptionPane.showInputDialog("Digite terceiro numero"));
+        int numero04 = Integer.parseInt(JOptionPane.showInputDialog("Digite quarto numero"));
+
+        if(numero01 == numero02){
+            JOptionPane.showMessageDialog(null, "É um quadrado");
+        }else {
+            JOptionPane.showMessageDialog(null, "Não é um quandrado");
+        }
+
+    }
+}  
